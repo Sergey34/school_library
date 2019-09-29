77e5d8ec5f67af95abdbff951683a16b9fba373e
Sergey34 <Sergey3440@gmail.com>
2019-09-29 13:37:53 +0400

    update image

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -3,10 +3,9 @@ var tipuesearch = {
         {
             "title": "Никольский",
             "text": "Tipue is a small web development studio based in North London.",
-            "image": "",
+            "image": "/school_library/files/books/5/themes/math/ozzy.jpeg",
             "tags": "5class Никольский",
-            "note": "asdasd",
-            "url": "http://www.tipue.com"
+            "url": "/school_library/files/books/5/themes/math/5_class_nikolskiy_2015.pdf"
         }
     ]
 };
