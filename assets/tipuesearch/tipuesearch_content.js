170af560048c9c6e3cf8570ad2d836a7780318a0
Sergey34 <Sergey3440@gmail.com>
2019-09-29 13:17:35 +0400

    update context

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -3,7 +3,7 @@ var tipuesearch = {
         {
             "title": "Tipue",
             "text": "Tipue is a small web development studio based in North London.",
-            "tags": "1class, 2class",
+            "tags": "Никольский",
             "url": "http://www.tipue.com"
         },
         {
