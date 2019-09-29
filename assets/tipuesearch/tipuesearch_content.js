bc50f852df6b79053e1b7393bfb2da72471429ec
Sergey34 <Sergey3440@gmail.com>
2019-09-29 13:20:45 +0400

    update context

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -1,16 +1,21 @@
 var tipuesearch = {
     "pages": [
         {
-            "title": "Tipue",
+            "title": "Никольский",
             "text": "Tipue is a small web development studio based in North London.",
             "tags": "Никольский",
+            "url": "http://www.tipue.com"
+        }, {
+            "title": "Nikol'skij",
+            "text": "Tipue is a small web development studio based in North London.",
+            "tags": "Nikol'skij",
             "url": "http://www.tipue.com"
         },
         {
             "title": "Tipue Search, a site search engine jQuery plugin",
             "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source, responsive and fast. Tipue Search only needs a browser that supports jQuery. It doesn't need MySQL or similar. In Static mode it doesn't even need a web server.",
             "tags": "5class",
-            "url": "/files/books/5/themes/math/5_class_nikolskiy_2015.pdf"
+            "url": "/school_library/files/books/5/themes/math/5_class_nikolskiy_2015.pdf"
         }
     ]
 };
